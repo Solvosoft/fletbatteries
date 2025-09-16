3853deea2cd8f7e37b3be7d94aa14ca69d33c245
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2025-09-16 12:57:33 -0600

    Integrate Supabase client to fetch event data for Calendar; update Calendar component to display events

--- src/views/calendar_view.py
+++ src/views/calendar_view.py
@@ -1,9 +1,22 @@
 import flet as ft
 from components.shared.calendar import Calendar
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from supabase import create_client, Client
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 def build_view_calendar(page):
+
+    response = (
+    supabase.table("Events")
+    .select("*")
+    .execute()
+)
     return ft.Container(
-        content=Calendar(),
+        content=Calendar(response.data),
         expand=True,
     )
 
